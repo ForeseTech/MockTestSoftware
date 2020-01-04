c69ed430b3f2edf03224f57f41cc851ec582735f
Arjun Aravind <arjun.aravind1998@gmail.com>
2020-01-04 08:39:57 +0000

    Modify sql-connections.php; Add .gitignore.

--- src/sql-connections.php
+++ src/sql-connections.php
@@ -4,12 +4,15 @@
 /* SQL-CONNECTIONS.PHP */
 /***********************/
 
+require "sql-variables.php";
+
 /* This function inits a connection variable and returns it. */
 function getConnection(){
 	$server = "localhost";
-	$username = getenv("MOCKS_USERNAME");
-	$password = getenv("MOCKS_PASSWORD");
-	$database = "MOCKS19";
+	$username = getDBUsername();
+	$password = getDBPassword();
+	$database = getDBName();
+
 	$connection=new PDO("mysql:host=$server;dbname=$database", $username, $password);
 	$connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	return $connection;
