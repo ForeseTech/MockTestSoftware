f512985354b861581232d48ad0bee39cf7669f5c
Arjun Aravind <arjun.aravind1998@gmail.com>
2020-02-11 13:50:09 +0000

    Updates to the CSV Generation file.

--- src/csv-generation.php
+++ src/csv-generation.php
@@ -6,16 +6,16 @@
 
 function generateCSV() {
 
-	$csv = "REG_NO,NAME,DEPT,EMAIL,SET_NO,SEC_1,SEC_2,SEC_3,SEC_4,SEC_5,TOTAL_SCORE\n";
+	$csv = "REG_NO,NAME,DEPT,TOTAL_SCORE\n";
 
 	try{
 	    
 		$conn = getConnection();	
-		$sql_stmt = "SELECT REG_NO, NAME, DEPT, EMAIL, SET_NO, SEC_1, SEC_2, SEC_3, SEC_4, SEC_5, TOTAL_SCORE FROM SCORES ORDER BY DEPT, TOTAL_SCORE DESC;";
+		$sql_stmt = "SELECT REG_NO, NAME, DEPT, TOTAL_SCORE FROM SCORES ORDER BY DEPT, TOTAL_SCORE DESC;";
 		$results = executeQuery($conn, $sql_stmt);
 
 		foreach($results as $row) {
-			$csv .= $row['REG_NO'].",".$row['NAME'].",".$row['DEPT'].",".$row['EMAIL'].",".$row['SET_NO'].",".$row['SEC_1'].",".$row['SEC_2'].",".$row['SEC_3'].",".$row['SEC_4'].",".$row['SEC_5'].",".$row['TOTAL_SCORE']."\n";
+			$csv .= $row['REG_NO'].",".$row['NAME'].",".$row['DEPT'].",".$row['TOTAL_SCORE']."\n";
 		}
 
 	} catch (PDOExcpetion $e){
